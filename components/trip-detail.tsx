683380d740be7d4fa8b6ef866f96d394190809b8
Sanan Ahmad <sananahmad909@gmail.com>
2025-10-24 20:27:04 +0500

    3 hours

--- components/trip-detail.tsx
+++ components/trip-detail.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Trip, Location } from "@/app/generated/prisma";
+import { Location, Trip } from "@/app/generated/prisma";
 import Image from "next/image";
 import { Calendar, MapPin, Plus } from "lucide-react";
 import Link from "next/link";
@@ -8,7 +8,7 @@ import { Button } from "./ui/button";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "./ui/tabs";
 import { useState } from "react";
 import Map from "@/components/map";
-//import SortableItinerary from "./sortable-itinerary";
+import SortableItinerary from "./sortable-itenerary";
 
 export type TripWithLocation = Trip & {
   locations: Location[];
@@ -52,7 +52,7 @@ export default function TripDetailClient({ trip }: TripDetailClientProps) {
           </div>
         </div>
         <div className="mt-4 md:mt-0">
-          <Link href={`/trips/${trip.id}/itenerary/new`}>
+          <Link href={`/trips/${trip.id}/itinerary/new`}>
             <Button>
               {" "}
               <Plus className="mr-2 h-5 w-5" /> Add Location
@@ -110,7 +110,7 @@ export default function TripDetailClient({ trip }: TripDetailClientProps) {
               <div className="h-72 rounded-lg overflow-hidden shadow">
                 <Map itineraries={trip.locations} />
               </div>
-              {/* {trip.locations.length === 0 && (
+              {trip.locations.length === 0 && (
                 <div className="text-center p-4">
                   <p>Add locations to see them on the map.</p>
                   <Link href={`/trips/${trip.id}/itinerary/new`}>
@@ -120,7 +120,7 @@ export default function TripDetailClient({ trip }: TripDetailClientProps) {
                     </Button>
                   </Link>
                 </div>
-              )} */}
+              )}
 
               <div>
                 <p className="text-gray-600 leading-relaxed">
@@ -135,7 +135,7 @@ export default function TripDetailClient({ trip }: TripDetailClientProps) {
               <h2 className="text-2xl font-semibold"> Full Itinerary</h2>
             </div>
 
-            {/* {trip.locations.length === 0 ? (
+            {trip.locations.length === 0 ? (
               <div className="text-center p-4">
                 <p>Add locations to see them on the itinerary.</p>
                 <Link href={`/trips/${trip.id}/itinerary/new`}>
@@ -146,16 +146,15 @@ export default function TripDetailClient({ trip }: TripDetailClientProps) {
                 </Link>
               </div>
             ) : (
-              <div>Nothing for now</div>
-                <SortableItinerary locations={trip.locations} tripId={trip.id} />
-            )} */}
+              <SortableItinerary locations={trip.locations} tripId={trip.id} />
+            )}
           </TabsContent>
 
           <TabsContent value="map" className="space-y-6">
             <div className="h-72 rounded-lg overflow-hidden shadow">
-              {/* <Map itineraries={trip.locations} /> */}
+              <Map itineraries={trip.locations} />
             </div>
-            {/* {trip.locations.length === 0 && (
+            {trip.locations.length === 0 && (
               <div className="text-center p-4">
                 <p>Add locations to see them on the map.</p>
                 <Link href={`/trips/${trip.id}/itinerary/new`}>
@@ -165,7 +164,7 @@ export default function TripDetailClient({ trip }: TripDetailClientProps) {
                   </Button>
                 </Link>
               </div>
-            )} */}
+            )}
           </TabsContent>
         </Tabs>
       </div>
